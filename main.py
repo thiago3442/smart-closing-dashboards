50df3284cd09efe28bb3df2cd1ff8f2b341d75ce
Thiago Galdino Amorim <78427117+thiago3442@users.noreply.github.com>
2025-03-14 18:22:41 -0300

    Update main.py
    Addedd changes to take it the "Running" message everytime you change something in the PyGWalker graphs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pygwalker.api.streamlit import StreamlitRenderer
 import pandas as pd
 import streamlit as st
- 
+
 # Adjust the width of the Streamlit page
 st.set_page_config(
     page_title="Smart Closing Dashboards",
@@ -15,8 +15,9 @@ uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls"])
 
 # Process the file if uploaded
 if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file)
+    if "df" not in st.session_state:
+        st.session_state.df = pd.read_excel(uploaded_file)
 
-    pyg_app = StreamlitRenderer(df)
+    pyg_app = StreamlitRenderer(st.session_state.df)
     
     pyg_app.explorer()
